b3429a6597a7a08e6ea13de958f410ebd70fe575
unknown <parv.p90@gmail.com>
2021-10-03 17:17:45 +0530

    Changing the direction from radio button to number input
    * This way users can set degree for the color direction and make your app more dynamic and versetile

--- backgroundgen.js
+++ backgroundgen.js
@@ -3,23 +3,11 @@ let color2 = document.getElementById("c2");
 let css = document.getElementById("code");
 let body = document.getElementById("gradient");
 let button = document.getElementById("random");
-let radioGroup = document.getElementById("radioIn");
-let radioButton = radioGroup.getElementsByTagName("input");
+let degree = document.getElementById("degree");
+degree.setAttribute('value', 90)
 let direction;
 
 //the event listner is to be added.
-for (let x = 0; x < radioButton.length; x++) {
-  radioButton[x].addEventListener("input", radioValue);
-}
-
-function radioValue() {
-  for (let j = 0; j < radioButton.length; j++) {
-    if (radioButton[j].checked) {
-      direction = radioButton[j].value;
-      //console.log(direction);
-    }
-  }
-}
 function getRandomColor() {
   let letters = "0123456789ABCDEF";
   let color = "#";
@@ -27,11 +15,12 @@ function getRandomColor() {
   return color;
 }
 
+
 function setgradient() {
   body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    "linear-gradient(" + degree.value + "deg ," + color1.value + ", " + color2.value + ")";
   css.textContent = body.style.background + ";";
-  console.log(body.style.background);
+  // console.log(body.style.background);
 }
 
 function randomgradient() {
@@ -40,12 +29,13 @@ function randomgradient() {
   color1.value = randomColor1;
   color2.value = randomColor2;
   body.style.background =
-    "linear-gradient(to right, " + randomColor1 + ", " + randomColor2 + ")";
+    "linear-gradient(" + degree.value + "deg ," + randomColor1 + ", " + randomColor2 + ")";
   css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setgradient);
 color2.addEventListener("input", setgradient);
+degree.addEventListener("input", setgradient);
 button.addEventListener("click", randomgradient);
 
 window.onload = randomgradient();
